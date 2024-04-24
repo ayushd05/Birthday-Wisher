f48eaaf523d3e76ab6f1dac24d4c3d90870a093f
Your Name <dixitayush550@gmail.com>
2024-04-24 11:38:52 +0530

    Automatic Birthday Wisher: Python Project

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import datetime
+import smtplib
+
+#Enter Authentication Details
+GMAIL_ID = 'dixitayush550@gmail.com'
+GMAIL_PSWD = 'ufyd cgoq tzum rbrn'
+
+
+def sendEmail(to,sub,msg):
+    print(f"Email to {to} sent with subject: {sub} and message {msg}")
+    s= smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(GMAIL_ID, GMAIL_PSWD)
+
+    s.sendmail(GMAIL_ID, to, f"subject: {sub}\n\n{msg}")
+    s.quit()
+
+
+
+if __name__ == "__main__":
+    # sendEmail(GMAIL_ID, 'subject','test message')
+    df = pd.read_excel("data.xlsx")
+    # print(df)
+    today =datetime.datetime.now().strftime("%d-%m")
+    yearNow= datetime.datetime.now().strftime("%Y")
+    # print(today)
+    writeInd = []
+    for index, item in df.iterrows():
+        print(index, item['Birthday'])
+        bday =item['Birthday'].strftime("%d-%m")
+        print(bday)
+        if (today == bday) and yearNow not in str(item['Year']):
+            sendEmail(item['Email'], "Happy Birthday", item['Dialogue'])
+            writeInd.append(index)
+
+
+    print(writeInd)
+    for i in writeInd:
+        yr= df.loc[i,'Year']
+        df.loc[i, 'Year']= str(yr) +', '+str(yearNow)
+        print(df.loc[i,'Year'])    
+    print(df)
+    df.to_excel('data.xlsx', index=False)
